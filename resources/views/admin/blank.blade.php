eb08f829cb4adf6a2666461e58cbdab105f198ce
Ammaday11 <ammaday7447@gmail.com>
2023-10-01 18:19:21 +0500

    Add Navbar and Sidebar

---
+++ resources/views/admin/blank.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.dashboard')
+@section('content')
+<body> 
+
+    
+        <!-- ============================================================== -->
+        <!-- wrapper  -->
+        <!-- ============================================================== -->
+        <div class="dashboard-wrapper">
+            <div class="container-fluid dashboard-content">
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <h3 class="text-center">Content goes here!</h3>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            @include('include.footer')
+            <!-- ============================================================== -->
+            <!-- end footer -->
+            <!-- ============================================================== -->
+        </div>
+    
+</body>
+    <!-- Optional JavaScript -->
+@endsection
