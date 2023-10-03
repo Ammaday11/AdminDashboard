3c2acb4d64642438787699ff3920c3fd577617b5
Ammaday11 <ammaday7447@gmail.com>
2023-10-03 18:20:47 +0500

    Created Homepage

--- resources/views/admin/blank.blade.php
+++ resources/views/admin/blank.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.dashboard')
 @section('style')
-{{-- <link rel="stylesheet" href="../resources/assets/assets/vendor/charts/c3charts/c3.css"> --}}
 <link rel="stylesheet" href="../resources/assets/assets/vendor/datatables/css/dataTables.bootstrap4.css">
 <link rel="stylesheet" href="../resources/assets/assets/vendor/datatables/css/buttons.bootstrap4.css">
 <link rel="stylesheet" href="../resources/assets/assets/vendor/datatables/css/select.bootstrap4.css">
@@ -17,170 +16,18 @@
             <div class="container-fluid dashboard-content">
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <!-- ============================================================== -->
+                        <!-- Start Content  -->
+                        <!-- ============================================================== -->
                         <h3 class="text-left">{{ now()->format('Y-M-d') }}</h3>
-                        <div class="row">
-                            <!-- ============================================================== -->
-                            <!-- four widgets   -->
-                            <!-- ============================================================== -->
-                            
-                            <!-- ============================================================== -->
-                            <!-- total followers   -->
-                            <!-- ============================================================== -->
-                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="d-inline-block">
-                                            <h5 class="text-muted">Pending</h5>
-                                            <h2 class="mb-0"> 3</h2>
-                                        </div>
-                                        <div class="float-right icon-circle-medium  icon-box-lg  bg-brand-light mt-1">
-                                            <i class=" fas fa-clock fa-fw fa-sm text-brand"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- ============================================================== -->
-                            <!-- end total followers   -->
-                            <!-- ============================================================== -->
-                            <!-- ============================================================== -->
-                            <!-- partnerships   -->
-                            <!-- ============================================================== -->
-                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="d-inline-block">
-                                            <h5 class="text-muted">Uncleared</h5>
-                                            <h2 class="mb-0">4</h2>
-                                        </div>
-                                        <div class="float-right icon-circle-medium  icon-box-lg  bg-secondary-light mt-1">
-                                            <i class=" fa fa-exclamation-triangle fa-fw fa-sm text-secondary"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- ============================================================== -->
-                            <!-- end partnerships   -->
-                            <!-- ============================================================== -->
-                            <!-- ============================================================== -->
-                            <!-- partnerships   -->
-                            <!-- ============================================================== -->
-                            {{-- <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="d-inline-block">
-                                            <h5 class="text-muted">Testing</h5>
-                                            <h2 class="mb-0">4</h2>
-                                        </div>
-                                        <div class="float-right icon-circle-medium  icon-box-lg  bg-alert-light mt-1">
-                                            <i class=" fa fa-exclamation-triangle fa-fw fa-sm text-alert"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> --}}
-                            <!-- ============================================================== -->
-                            <!-- end partnerships   -->
-                            <!-- ============================================================== -->
-                            <!-- ============================================================== -->
-                            <!-- total earned   -->
-                            <!-- ============================================================== -->
-                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="d-inline-block">
-                                            <h5 class="text-muted">Completed</h5>
-                                            <h2 class="mb-0"> 5</h2>
-                                        </div>
-                                        <div class="float-right icon-circle-medium  icon-box-lg  bg-success-light mt-1">
-                                            <i class="fas fa-check fa-fw fa-sm text-success"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- ============================================================== -->
-                            <!-- end total earned   -->
-                            <!-- ============================================================== -->
-                            <!-- ============================================================== -->
-                            <!-- total views   -->
-                            <!-- ============================================================== -->
-                            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="d-inline-block">
-                                            <h5 class="text-muted">Total Orders</h5>
-                                            <h2 class="mb-0"> 12</h2>
-                                            <h5 class="text-muted">Total Covers</h5>
-                                            <h2 class="mb-0"> 15</h2>
-                                        </div>
-                                        <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
-                                            <i class="fa fa-eye fa-fw fa-sm text-info"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- ============================================================== -->
-                            <!-- end total views   -->
-                            <!-- ============================================================== -->
-                        </div>
+                        <h3 class="text-center">Content goes here!</h3>
+
+
+
 
                         <!-- ============================================================== -->
-                <!-- data table  -->
-                <!-- ============================================================== -->
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <a href="#" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">New Order</a>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table id="example" class="table table-striped table-bordered second" style="width:100%">
-                                    <thead>
-                                        <tr>
-                                            <th>Check</th>
-                                            <th>Room</th>
-                                            <th>Cover</th>
-                                            <th>Time</th>
-                                            <th>Order Taker</th>
-                                            <th>Details</th>
-                                            <th>Delivered By</th>
-                                            <th>Delivered At</th>
-                                            <th>Cleared By</th>
-                                            <th>Cleared At</th>
-                                            <th>Status</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {{-- @foreach ($teachers as $teacher) --}}
-                                        <tr>
-                                            <td>Test1</td>
-                                            <td>Test2</td>
-                                            <td>Test3</td>
-                                            <td>Test4</td>
-                                            <td>Test5</td>
-                                            <td>Test6</td>
-                                            <td>Test7</td>
-                                            <td>Test8</td>
-                                            <td>Test9</td>
-                                            <td>Test10</td>
-                                            <td>Test11</td>
-                                            <td>
-                                                <a href="#" class="btn btn-brand btn-sm m-r-10 fas fa-edit" data-toggle="modal" data-target="#exampleModal"></a>
-                                                <a href="#" class="btn btn-danger btn-sm m-r-10 fas fa-trash" data-toggle="modal" data-target="#exampleModal"></a>
-                                            </td>
-                                        </tr>
-                                    {{-- @endforeach --}}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- ============================================================== -->
-                <!-- end data table  -->
-                <!-- ============================================================== -->
-                
-                        <h3 class="text-center">Content goes here!</h3>
-                        
+                        <!-- End Content  -->
+                        <!-- ============================================================== -->
                     </div>
                 </div>
             </div>
@@ -198,9 +45,6 @@
             <!-- ============================================================== -->
         </div>
     @section('script')
-        {{-- <script src="../resources/assets/assets/vendor/charts/c3charts/c3.min.js"></script>
-        <script src="../resources/assets/assets/vendor/charts/c3charts/d3-5.4.0.min.js"></script>
-        <script src="../resources/assets/assets/vendor/charts/c3charts/C3chartjs.js"></script> --}}
         <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
         <script src="../resources/assets/assets/vendor/datatables/js/dataTables.bootstrap4.min.js"></script>
         <script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
@@ -217,5 +61,4 @@
         <script src="https://cdn.datatables.net/fixedheader/3.1.5/js/dataTables.fixedHeader.min.js"></script>
     @endsection
 </body>
-    <!-- Optional JavaScript -->
 @endsection
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,7 +5,7 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Concept - Bootstrap 4 Admin Dashboard Template</title>
+        <title>Order Status</title>
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href= "../resources/assets/assets/vendor/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href= "../resources/assets/assets/vendor/fonts/circular-std/style.css">
